0f47eb47dbfc6abe2df07ba2145ffdbdea02cc4c
Cristian Marius Valentin <61987293+UnStudentRoman@users.noreply.github.com>
2021-12-20 22:10:15 +0200

    Project no.1

---
+++ Day_1/Band_name_generator.py
@@ -0,0 +1,10 @@
+#Band Name Generator
+
+print("Welcome to the band name generator!")
+
+
+city_name = input("What's the name of the city you grew up in?\n")
+
+favourite_food = input("What's your favourite food?\n")
+
+print("Your band name can be " + city_name + " " + favourite_food)
